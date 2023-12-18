7771fa45e9fd3590f806b4afe418f67b45eb1b71
achrafabbes99 <achraf_ab1999@hotmail.fr>
2023-12-18 11:37:56 +0100

    Backoffice

--- src/main/java/com/fassarly/academy/controllers/MatiereController.java
+++ src/main/java/com/fassarly/academy/controllers/MatiereController.java
@@ -5,6 +5,7 @@ import com.fassarly.academy.services.MatiereServiceImpl;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/fassarly/academy/controllers/UtilisateurController.java
+++ src/main/java/com/fassarly/academy/controllers/UtilisateurController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/utilisateur")
 @AllArgsConstructor
+@CrossOrigin("http://localhost:4200/")
 public class UtilisateurController {
 
     UtilisateurServiceImpl utilisateurService;
